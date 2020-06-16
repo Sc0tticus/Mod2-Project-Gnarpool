db392e039d9d679c053788d7c1ef217f27b7444b
jdbuysse <jdbuysse@gmail.com>
2020-06-16 11:08:34 -0600

    added filtering to drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,8 +1,21 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all 
+    if params[:resort]
+      @drivers = Driver.where(resort: params[:resort])
+      render json: @drivers
 
-    render json: @drivers
+    elsif params[:pass]
+      @drivers = Driver.where(pass: params[:pass])
+      render json: @drivers
+
+    elsif params[:date]
+      @drivers = Driver.where(date: params[:date])
+      render json: @drivers
+
+    else
+      @drivers = Driver.all 
+      render json: @drivers
+    end
   end
 
   def show
@@ -11,5 +24,18 @@ class DriversController < ApplicationController
     render json: @driver, inlcude: [:rides]
   end
 
+  def create 
+    @driver = Driver.create(
+        name: params[:name],
+        phone: params[:phone],
+        email: params[:email],
+        date: params[:date],
+        time: params[:time],
+        resort: params[:resort],
+        pass: params[:pass],
+        VenMo: params[:VenMo],
+    )
+    redirect_to 'http://localhost:3001/index.html'
+end
 
 end
